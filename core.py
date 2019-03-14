d983d4f13f99bcc73f88a4ef7d2193d26a7869c5
Felgrand292 <46786112+Felgrand292@users.noreply.github.com>
2019-03-14 07:17:01 +0000

    V0.3
    Added Clock.
    Added Daily reminder function.
    More commenting

--- core.py
+++ core.py
@@ -1,14 +1,17 @@
 # Python Program to respond to text based spoken language type commands and generate varied responses
 # Also to help organisation!
 # NAME = "Alfred"
+import os
 from os import path																					#Imports and set-up's
 import time
 import datetime
+import calendar
 import sqlite3
 from sqlite3 import Error
 from random import *
 from settings import *
 from speechChoiceArrays import *
+from threading import Thread
 
 class Core:
 	def __init__(self):
@@ -16,11 +19,34 @@ class Core:
 		print("Upon initialization;")
 		i = randrange(0,len(helloOps))																#Selects random number for respone at the range of 0 to the length of the speech options
 		self.currentUpdate()
-
+		self.todo()
 		print("")																					#Starts statement cycle of the program, ends initialization
 		print(helloOps[i])
-		self.idnum = "2"
+		t = Thread(target=self.getTime)																#Starts a parallel function in the background that becomes the clock.
+		t.start()
 		self.input()
+
+
+	def getTime(self):																				#Used in background clock
+		timer = True
+		while timer:
+			now = datetime.datetime.now()															#Loop running in background constantly updates to the current time
+			self.currenttime = now.strftime("%H:%M")
+
+	def todo(self):
+		day = datetime.datetime.now()
+		self.currentday = day.strftime("%A")
+		for i in range (0,6):
+			if self.currentday == calendar.day_name[i]:
+				self.currentdayint = i
+		print("Today is "+self.currentday+", sir.")
+		cur.execute("SELECT TITLE FROM DAILY WHERE DATEPART='"+self.currentday+"'");
+		self.daily = cur.fetchall()
+		print("For today, sir, you have: ")
+		for i in range (0,len(self.daily)):
+			print(str(self.daily[i][0])+".")
+		print("Good luck, sir")
+		print(day.strftime("%H:%M")+"lala")
 
 	def input(self):
 		i = randrange(0,len(statementTakingOps))
@@ -36,7 +62,7 @@ class Core:
 				self.introduce()
 				self.input()
 		if ("new" or "make") and "event" in self.statment:
-			self.title = input("What for, sir? ")
+			self.title = input("What for, sir? ")													#>>GIVE ALL OPTIONS<<
 			self.date = str(input("And when will this be for? "))
 			self.who = input("With anyone in particular? ")
 			if "no" in self.who:
@@ -53,14 +79,30 @@ class Core:
 		if "new" and "daily" in self.statment:
 			self.newReminder()
 		input("Error with statement")
+		time.sleep(1)
+		print("Restarting...")
+		time.sleep(1)
+		os.system('cls')
+		self.input()
 	def currentUpdate(self):
 		now = datetime.datetime.now()
 		i = randrange(0,len(timeUpdateOps))
 		print("The current date and time "+timeUpdateOps[i]+":"+now.strftime("%Y-%m-%d %H:%M"))
 	def introduce(self):
 		print(helloOps[randrange(0,len(helloOps))]+nameOps[randrange(0,len(nameOps))]+NAME+"!")
+	def newReminder(self):
+		self.title = input("What is the title sir? ")
+		self.day = input("What day for, sir? ")
+		self.time = input("Would you like to set a time? ")
+		if "no" in self.time:
+			self.time = "NULL"
+		conn.execute("INSERT INTO DAILY (TITLE, DATEPART, TIMEPART) \
+			VALUES('"+self.title+"','"+self.day+"','"+self.time+"')");
+		conn.commit()
+		print("New daily reminder created for "+self.day+", sir.")
 
 conn = sqlite3.connect('storage.db')																	#Database Initialization
+cur = conn.cursor()
 conn.execute('''CREATE TABLE IF NOT EXISTS EVENTS
 		(ID INTEGER PRIMARY KEY		NOT NULL,
 		TITLE 			TEXT 	NOT NULL,
@@ -68,6 +110,11 @@ conn.execute('''CREATE TABLE IF NOT EXISTS EVENTS
 		DESCRIPTION 	CHAR(150),
 		WITH 			CHAR(30),
 		ADDRESS			CHAR(250));''')	#For making new databases
+conn.execute('''CREATE TABLE IF NOT EXISTS DAILY
+		(ID INTEGER PRIMARY KEY		NOT NULL,
+		TITLE 			TEXT 	NOT NULL,
+		DATEPART 		TEXT 	NOT NULL,
+		TIMEPART 		TEXT);''')
 
 c = Core()
 """ TO-DO
